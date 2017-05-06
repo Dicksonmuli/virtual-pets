d8834682fe888249f5e3ba7d71c278cdb9e8d601
dicksonmuli <dicksonmully6@gmail.com>
2017-05-06 17:47:05 +0300

    creating and connecting to the database

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -3,6 +3,43 @@ public class Person {
 	private String name;
 
 	public Person(String name, String email) {
-		
+		this.name = name;
+		this.email = email;
+
+		try(Connection con = DB.sql2o.open()) {
+			String sql = "INSERT INTO persons(name, email) VALUES (:name, :email)";
+			con.createQuery(sql)
+			.addParameter("name", this.name)
+			.executeUpdate();
+		}
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	//overriding equals
+	@Override
+ public boolean equals(Object otherPerson){
+	 if (!(otherPerson instanceof Person)) {
+		 return false;
+	 } else {
+		 Person newPerson = (Person) otherPerson;
+		 return this.getName().equals(newPerson.getName()) &&
+						this.getEmail().equals(newPerson.getEmail());
+	 }
+ }
+ //saving to the db
+ public void save() {
+	 try(Connection con = DB.sql2o.open()) {
+		 String sql = "INSERT INTO persons(name, email) VALUES (:name, :email)";
+		 con.createQuery(sql)
+		 .addParameter("name", this.name)
+		 .executeUpdate();
+	 }
+ }
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -3,6 +3,10 @@ import static org.junit.Assert.*;
 
 public class PersonTest {
 	private Person testPerson;
+
+	@Rule
+	public DatabaseRule database = new DatabaseRule();
+
 	@Before
 	public void instance() {
 		testPerson = new Person("Henry", "[email protected]");
@@ -14,9 +18,21 @@ public class PersonTest {
 	}
 	@Test
 	public void getName_personInstantiatesWithName_Henry() {
-		assertEqual("Henry", testPerson.getName());
+		assertEquals("Henry", testPerson.getName());
 	}
 	public void getEmail_personInstantiatesWithEmail_String() {
-		assertEqual("[email protected]", testPerson.getEmail());
+		assertEquals("[email protected]", testPerson.getEmail());
 	}
+	@Test public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+		Person firstPerson = new Person("Henry", "[email protected]");
+		Person anotherPerson = new Person("Henry", "[email protected]");
+		assertTrue(firstPerson.equals(anotherPerson));
+	}
+	//saving to the db
+	@Test
+	public void save_insertsObjectIntoDatabase_Person() {
+		testPerson.save();
+		assertTrue(Person.all().get(0).equals(testPerson));
+	}
+	//returning a
 }
