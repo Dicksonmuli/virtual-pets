3e8f784c91012dae0de60d7e809b29322da0c841
dicksonmuli <dicksonmully6@gmail.com>
2017-05-06 20:01:40 +0300

    retrieving all monsters from db belonging to a person

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -58,5 +58,13 @@ public class Monster {
 		 return monster;
 	 }
  }
+ public List<Monster> getMonsters() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM monsters where personId=:id";
+      return con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeAndFetch(Monster.class);
+    }
+  }
 
 }
