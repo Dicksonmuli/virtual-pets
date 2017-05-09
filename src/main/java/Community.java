61c2cc7e619539854ac346de2e364d3165852750
dicksonmuli <dicksonmully6@gmail.com>
2017-05-09 10:48:27 +0300

    community instantiates with a name and a description

--- src/main/java/Community.java
+++ src/main/java/Community.java
@@ -3,6 +3,17 @@ public class Community {
   private String description;
   private int id;
 
+
   public Community(String name, String description){
+		this.name = name;
+		this.description = description;
+}
+
+//returns name
+public String getName() {
+	return name;
+}
+public String getDescription() {
+	return description;
 }
 }
--- src/test/java/CommunityTest.java
+++ src/test/java/CommunityTest.java
@@ -13,5 +13,16 @@ public class CommunityTest {
     Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters!");
     assertEquals(true, testCommunity instanceof Community);
   }
-
+  //instanciates with name
+  @Test
+ public void getName_communityInstantiatesWithName_true() {
+   Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters!");
+   assertEquals("Water Enthusiasts", testCommunity.getName());
+ }
+ //instanciates with a description
+ @Test
+ public void getDescription_communityInstantiatesWithDescription_String() {
+   Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters!");
+   assertEquals("Lovers of all things water monsters!", testCommunity.getDescription());
+ }
 }
